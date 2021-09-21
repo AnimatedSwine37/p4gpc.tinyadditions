ec86bf74c3bbf1745ef6340b32e04568eed72024
bulby <benharless820@gmail.com>
2021-09-21 08:20:42 -0400

    auto advance toggle but clean

--- p4gpc.tinyadditions/AutoAdvanceToggle.cs
+++ p4gpc.tinyadditions/AutoAdvanceToggle.cs
@@ -7,20 +7,20 @@ namespace p4gpc.tinyadditions
 {
     class AutoAdvanceToggle
     {
-        private ILogger _logger;
+        private Utils _util;
         private IReloadedHooks _hooks;
         private IMemory _memory;
         private int _baseAddress;
         public Config Configuration {get; set;}
 
-        public AutoAdvanceToggle(ILogger logger, int baseAddress, Config configuration, IMemory memory,IReloadedHooks hooks)
+        public AutoAdvanceToggle(Utils util, int baseAddress, Config configuration, IMemory memory,IReloadedHooks hooks)
         {
             Configuration = configuration;
-            _logger = logger;
+            _util = util;
             _hooks = hooks;
             _memory = memory;
             _baseAddress = baseAddress;
-            _logger.WriteLine("[TinyAdditons] Initizalizing auto-advance toggle");
+            _util.Log("Initizalizing auto-advance toggle");
         }
         public void ToggleAutoAdvance() {
             try
@@ -30,10 +30,10 @@ namespace p4gpc.tinyadditions
                 autoAdvance ^= 0b00001000;
 
                 _memory.SafeWrite((IntPtr)(_baseAddress + 0x49DD563), ref autoAdvance);
-                _logger.WriteLine("[TinyAdditions] Toggled Auto Advance");
+                _util.Log("Toggled Auto Advance");
             } catch (Exception e)
             {
-                _logger.WriteLine($"[TinyAdditions] Couldn't Read or write address for auto advance toggle {e.Message}");
+                _util.Log($"Couldn't Read or write address for auto advance toggle {e.Message}");
             }
         }
     }
