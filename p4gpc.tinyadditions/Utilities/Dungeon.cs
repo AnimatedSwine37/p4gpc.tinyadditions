42d18242fb2744ca62df3fc395560249be4bd224
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-04-06 08:39:26 +1000

    Add utility Dungeon class

---
+++ p4gpc.tinyadditions/Utilities/Dungeon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p4gpc.tinyadditions.Utilities
+{
+    public class Dungeon
+    {
+        public string Name { get; }
+        public int StartFloor { get; }
+        public int EndFloor { get; }
+
+        public Dungeon(string name, int startFloor, int endFloor)
+        {
+            Name = name;
+            StartFloor = startFloor;
+            EndFloor = endFloor;
+        }
+    }
+}
