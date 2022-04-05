fa046db9e9565ba1a27dbc9119c4cd18fff2f6de
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-04-06 08:38:03 +1000

    Remove overcomplicated way of doing things

--- p4gpc.tinyadditions/Additions/PersistentBGM.cs
+++ p4gpc.tinyadditions/Additions/PersistentBGM.cs
@@ -1,4 +1,5 @@
 ﻿using p4gpc.tinyadditions.Configuration;
+using p4gpc.tinyadditions.Utilities;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.Enums;
 using Reloaded.Hooks.Definitions.X86;
@@ -17,27 +18,13 @@ namespace p4gpc.tinyadditions.Additions
     {
         private IReverseWrapper<ShouldSwitchBgmFunction> _shouldSwitchBgmReverseWrapper;
 
-        private IAsmHook _bgmStartHook;
-        private IAsmHook _bgmEndHook;
-        private IAsmHook _battleStartingHook;
-        private IAsmHook _battleStartedHook;
-        private IAsmHook _bgmFadeHook;
-        private IAsmHook _battleEndingHook;
-        private IAsmHook _battleEndedHook;
         private List<IAsmHook> _hooksList;
 
-        private string _shouldSwitchBgmCall;
-
-        private IntPtr _skipBgmChange;
-        private IntPtr _battleEnding;
+        private GetFloorId _getFloorId;
 
         public PersistentBGM(Utils utils, int baseAddress, Config configuration, IMemory memory, IReloadedHooks hooks) : base(utils, baseAddress, configuration, memory, hooks)
         {
-            _skipBgmChange = _memory.Allocate(1);
-            _memory.Write(_skipBgmChange, false);
-            _battleEnding = _memory.Allocate(1);
-            _memory.Write(_battleEnding, false);
-            InitialiseHooks();
+            InitialiseGetFloorId();
         }
 
         public override void Resume()
@@ -53,172 +40,26 @@ namespace p4gpc.tinyadditions.Additions
         }
 
         /// <summary>
-        /// Initialises all of the hooks and adds them to a list of hooks
+        /// Initialises the wrapper for the native get floor id function
         /// </summary>
-        private void InitialiseHooks()
+        private void InitialiseGetFloorId()
         {
-            _shouldSwitchBgmCall = _hooks.Utilities.GetAbsoluteCallMnemonics(ShouldSwitchBgm, out _shouldSwitchBgmReverseWrapper);
-            _utils.RunAsync(InitBattleStartingHook);
-            _utils.RunAsync(InitBgmEndHook);
-            _utils.RunAsync(InitBgmStartHook);
-            _utils.RunAsync(InitBgmFadeHook);
-            _utils.RunAsync(InitBattleStartedHook);
-            _utils.RunAsync(InitBattleEndingHook);
-            _utils.RunAsync(InitBattleEndedHook);
-            _hooksList = new List<IAsmHook> { _bgmStartHook, _bgmEndHook, _bgmFadeHook, _battleStartedHook, _battleStartingHook, _battleEndingHook, _battleEndedHook };
+            long address = _utils.SigScan("B9 E0 BB ?? ?? E8 ?? ?? ?? ?? A1 ?? ?? ?? ?? 85 C0 75 ?? 33 C9 EB 06", "get floor id");
+            _getFloorId = _hooks.CreateWrapper<GetFloorId>(address, out IntPtr getFloorIdAddress);
         }
 
-        /// <summary>
-        /// Initialise the hook that will stop the bgm from ending when starting a battle
-        /// </summary>
-        private void InitBgmEndHook()
+        private List<Dungeon> _dungeons = new List<Dungeon>
         {
-            long address = _utils.SigScan("53 ?? ?? B9 ?? ?? ?? ?? 56 E8 ?? ?? ?? ?? 6B F3 70", "bgm end");
-            string[] function =
-            {
-                "use32",
-                $"{_hooks.Utilities.PushCdeclCallerSavedRegisters()}",
-                $"{_shouldSwitchBgmCall}",
-                // If we should switch the bgm jump to the end and let the code run as normal 
-                "cmp eax, 1",
-                "je endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-                // Return the function as we shouldn't switch bgm so we shouldn't start a new one
-                "ret",
-                "label endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-            };
-            _bgmEndHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _bgmEndHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialises the hook that will stop new bgm from starting
-        /// </summary>
-        private void InitBgmStartHook()
-        {
-            long address = _utils.SigScan("55 8B EC 51 53 56 8B F1 B9 ?? ?? ?? ?? 57 89 75 ?? E8 ?? ?? ?? ?? 69 05 ?? ?? ?? ?? 20 02 00 00", "bgm start");
-            string[] function =
-            {
-                "use32",
-                $"{_hooks.Utilities.PushCdeclCallerSavedRegisters()}",
-                $"{_shouldSwitchBgmCall}",
-                // If we should switch the bgm jump to the end and let the code run as normal 
-                "cmp eax, 1",
-                "je endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-                // Return the function as we shouldn't switch bgm so we shouldn't start a new one
-                "ret",
-                "label endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-            };
-            _bgmStartHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _bgmStartHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialises the hook that happens just before a battle starts
-        /// </summary>
-        private void InitBattleStartingHook()
-        {
-            long address = _utils.SigScan("E8 ?? ?? ?? ?? C7 47 ?? 05 00 00 00 E9 ?? ?? ?? ?? A1 ?? ?? ?? ??", "battle starting");
-            string[] function =
-            {
-                "use32",
-                $"mov byte [{_skipBgmChange}], 1",
-            };
-            _battleStartingHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteAfter).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _battleStartingHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialise the hook that happens just as a battle is starting
-        /// </summary>
-        private void InitBattleStartedHook()
-        {
-            long address = _utils.SigScan("E8 ?? ?? ?? ?? 8B 0D ?? ?? ?? ?? 8B 81 ?? ?? ?? ?? 8B B1 ?? ?? ?? ??", "battle started");
-            string[] function =
-            {
-                "use32",
-                // The battle has now started
-                $"mov byte [{_skipBgmChange}], 0"
-            };
-            _battleStartedHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteAfter).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _battleStartedHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialises the hook that stops the bgm from fading in and out
-        /// </summary>
-        private void InitBgmFadeHook()
-        {
-            long address = _utils.SigScan("55 8B EC 51 53 56 8B F1 B9 ?? ?? ?? ?? 57 8B FA 89 75 ??", "bgm fade");
-            string[] function =
-            {
-                "use32",
-                $"{_hooks.Utilities.PushCdeclCallerSavedRegisters()}",
-                $"{_shouldSwitchBgmCall}",
-                // If we should switch the bgm jump to the end and let the code run as normal 
-                "cmp eax, 1",
-                "je endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-                // Return the function as we shouldn't switch bgm so we shouldn't start a new one
-                "ret",
-                "label endHook",
-                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
-            };
-            _bgmFadeHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _bgmFadeHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialises the hook that stops the music from changing when the battle goes to the end screen stuff
-        /// </summary>
-        private void InitBattleEndingHook()
-        {
-            long address = _utils.SigScan("BA 07 00 00 00 ?? C9 E8 ?? ?? ?? ?? 8B 0D ?? ?? ?? ??", "battle ending");
-            string[] function =
-            {
-                "use32",
-                $"mov byte [{_battleEnding}], 1",
-                // Skip changing the music
-                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 18), false)}",
-            };
-            _battleEndingHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _battleEndingHook.Disable();
-        }
-
-        /// <summary>
-        /// Initialse the hook that will stop the music from switching once the battle has ended
-        /// </summary>
-        private void InitBattleEndedHook()
-        {
-            long address = _utils.SigScan("74 ?? 8B 77 ?? E8 ?? ?? ?? ??", "battle ended");
-            string[] function =
-            {
-                "use32",
-                // If the bgm wasn't going to be run in the first place skip this whole thing (there's a cmp just before the hook)
-                "jne endHook",
-                $"cmp byte [{_battleEnding}], 1",
-                "jne endHookResetComp",
-                $"mov byte [{_battleEnding}], 0",
-                // Skip the bgm call
-                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 30), false)}",
-                // End hook and reset the comparison to true
-                "label endHookResetComp",
-                "cmp eax, eax",
-                "label endHook"
-            };
-            _battleEndedHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
-            if (!_configuration.PersistentBgmEnabled)
-                _battleEndedHook.Disable();
-        }
+            new Dungeon("Castle", 5, 14),
+            new Dungeon("Bathhouse", 20, 32),
+            new Dungeon("Striptease", 40, 52),
+            new Dungeon("VoidQuest", 60, 71),
+            new Dungeon("Lab", 80, 90),
+            new Dungeon("Heaven", 100, 111),
+            new Dungeon("MagatsuInaba", 120, 130),
+            new Dungeon("YomotsuHirasaka", 140, 149),
+            new Dungeon("HollowForest", 160, 170)
+        };
 
         /// <summary>
         /// Checks if we should switch the bgm
@@ -226,12 +67,25 @@ namespace p4gpc.tinyadditions.Additions
         /// <returns>True if we should switch it, false if the current bgm should persist</returns>
         private bool ShouldSwitchBgm(int eax)
         {
-            _memory.Read(_skipBgmChange, out bool battleStarting);
-            // If in dungeon don't switch bgm
-            if (battleStarting)
+            int floorId = _getFloorId();
+            Dungeon? dungeon = _dungeons.FirstOrDefault(x => floorId > x.StartFloor && floorId <= x.EndFloor);
+            
+            // We're not in a known dungeon so just let the music through
+            if (dungeon == null)
+                return true;
+
+            _utils.LogDebug($"The floor id is {floorId} in the dungeon {dungeon.Name}");
+
+            Random random = new Random();
+            double randomNum = random.NextDouble();
+            float normalChance = (float)_configuration.GetType().GetProperty($"{dungeon.Name}NormalChance")!.GetValue(_configuration)!;
+            // We should let the bgm persist
+            if(randomNum > normalChance)
                 return false;
+            // Let the bgm change
             return true;
         }
+
 
         public override void UpdateConfiguration(Config configuration)
         {
@@ -246,5 +100,10 @@ namespace p4gpc.tinyadditions.Additions
         [Function(Register.eax, Register.eax, StackCleanup.Callee)]
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate bool ShouldSwitchBgmFunction(int eax);
+
+        [Function(Register.eax, Register.ecx, StackCleanup.Callee)]
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int GetFloorId();
+
     }
 }
