39538f03ae105e62a56519c9c26b20277833039d
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-04-08 10:33:25 +1000

    Add working persistent bgm

--- p4gpc.tinyadditions/Additions/PersistentBGM.cs
+++ p4gpc.tinyadditions/Additions/PersistentBGM.cs
@@ -18,13 +18,23 @@ namespace p4gpc.tinyadditions.Additions
     {
         private IReverseWrapper<ShouldSwitchBgmFunction> _shouldSwitchBgmReverseWrapper;
 
+        private IAsmHook _btlBgmStartHook;
+        private IAsmHook _btlEnterStopBgmHook;
+        private IAsmHook _resultsStartBgmHook;
+        private IAsmHook _afterResultsStopBgmHook;
         private List<IAsmHook> _hooksList;
+        private IntPtr _shouldSwitchBgm;
 
         private GetFloorId _getFloorId;
 
         public PersistentBGM(Utils utils, int baseAddress, Config configuration, IMemory memory, IReloadedHooks hooks) : base(utils, baseAddress, configuration, memory, hooks)
         {
+            _shouldSwitchBgm = _memory.Allocate(1);
             InitialiseGetFloorId();
+            InitBtlBgmStartHook();
+            InitBtlEnterStopBgmHook();
+            InitResultsStartBgmHook();
+            InitAfterResultsStopBgmHook();
         }
 
         public override void Resume()
@@ -48,6 +58,85 @@ namespace p4gpc.tinyadditions.Additions
             _getFloorId = _hooks.CreateWrapper<GetFloorId>(address, out IntPtr getFloorIdAddress);
         }
 
+        /// <summary>
+        /// Initialises the hook that controls battle bgm starting
+        /// </summary>
+        private void InitBtlBgmStartHook()
+        {
+            long address = _utils.SigScan("E8 ?? ?? ?? ?? 8B 0D ?? ?? ?? ?? 8B 81 ?? ?? ?? ?? 8B B1 ?? ?? ?? ??", "battle bgm start");
+            string[] function =
+            {
+                "use32",
+                $"cmp byte [{_shouldSwitchBgm}], 1",
+                // If we should switch bgm end the hook, running the original code
+                $"je endHook",
+                // Jump to the instruction after this start bgm call so it's skipped
+                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 5), false)}",
+                "label endHook",
+            };
+            _btlBgmStartHook = _hooks.CreateAsmHook(function, address - 2, AsmHookBehaviour.ExecuteFirst).Activate();
+        }
+
+        /// <summary>
+        /// Initialises the hook that controls regular bgm stopping when entering a battle
+        /// </summary>
+        private void InitBtlEnterStopBgmHook()
+        {
+            long address = _utils.SigScan("E8 ?? ?? ?? ?? 8B 15 ?? ?? ?? ?? A1 ?? ?? ?? ?? 0F 57 C0", "battle entering stop bgm");
+            string[] function =
+            {
+                "use32",
+                $"{_hooks.Utilities.PushCdeclCallerSavedRegisters()}",
+                $"{_hooks.Utilities.GetAbsoluteCallMnemonics(ShouldSwitchBgm, out _shouldSwitchBgmReverseWrapper)}",
+                "cmp eax, 1",
+                $"{_hooks.Utilities.PopCdeclCallerSavedRegisters()}",
+                // If we should switch bgm end the hook, running the original code
+                $"je endHook",
+                // Jump to the instruction after this start bgm call so it's skipped
+                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 5), false)}",
+                "label endHook",
+            };
+            _btlEnterStopBgmHook = _hooks.CreateAsmHook(function, address - 2, AsmHookBehaviour.ExecuteFirst).Activate();
+        }
+
+        /// <summary>
+        /// Initialises the hook that controls the results screen bgm starting
+        /// </summary>
+        private void InitResultsStartBgmHook()
+        {
+            long address = _utils.SigScan("33 C9 E8 ?? ?? ?? ?? 8B 0D ?? ?? ?? ?? 8B 81 ?? ?? ?? ?? 8B 40 ??", "results start bgm");
+            string[] function =
+            {
+                "use32",
+                $"cmp byte [{_shouldSwitchBgm}], 1",
+                // If we should switch bgm end the hook, running the original code
+                $"je endHook",
+                // Jump to the instruction after this start bgm call so it's skipped
+                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 7), false)}",
+                "label endHook",
+            };
+            _resultsStartBgmHook = _hooks.CreateAsmHook(function, address, AsmHookBehaviour.ExecuteFirst).Activate();
+        }
+
+        /// <summary>
+        /// Initialises the hook that controls bgm stoppping after the results screen is closed
+        /// </summary>
+        private void InitAfterResultsStopBgmHook()
+        {
+            long address = _utils.SigScan("E8 ?? ?? ?? ?? A1 ?? ?? ?? ?? 83 60 ?? FE", "after results stop bgm");
+            string[] function =
+            {
+                "use32",
+                $"cmp byte [{_shouldSwitchBgm}], 1",
+                // If we should switch bgm end the hook, running the original code
+                $"je endHook",
+                // Jump to the instruction after this start bgm call so it's skipped
+                $"{_hooks.Utilities.GetAbsoluteJumpMnemonics((IntPtr)(address + 5), false)}",
+                "label endHook",
+            };
+            _afterResultsStopBgmHook = _hooks.CreateAsmHook(function, address - 2, AsmHookBehaviour.ExecuteFirst).Activate();
+        }
+
         private List<Dungeon> _dungeons = new List<Dungeon>
         {
             new Dungeon("Castle", 5, 14),
@@ -67,6 +156,8 @@ namespace p4gpc.tinyadditions.Additions
         /// <returns>True if we should switch it, false if the current bgm should persist</returns>
         private bool ShouldSwitchBgm(int eax)
         {
+            _memory.Write(_shouldSwitchBgm, false);
+            return false;
             int floorId = _getFloorId();
             Dungeon? dungeon = _dungeons.FirstOrDefault(x => floorId > x.StartFloor && floorId <= x.EndFloor);
             
